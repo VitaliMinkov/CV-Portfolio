55237c77ed089114885f0e4688767fd343b7e6f4
VitaliMinkov <105166625+VitaliMinkov@users.noreply.github.com>
2026-01-28 19:56:26 +0400

    Add files via upload

---
+++ CV_dataset_description.sql
@@ -0,0 +1,37 @@
+
+-- TABLE: orders
+
+-- row_id            INTEGER
+-- order_id          TEXT
+-- order_date        DATE
+-- ship_date         DATE
+-- ship_mode         TEXT
+-- customer_id       TEXT
+-- customer_name     TEXT
+-- segment           TEXT
+-- country           TEXT
+-- city              TEXT
+-- state_province    TEXT
+-- postal_code       TEXT
+-- region            TEXT
+-- product_id        TEXT
+-- category          TEXT
+-- sub_category      TEXT
+-- product_name      TEXT
+-- sales             NUMERIC
+-- quantity          NUMERIC
+-- discount          NUMERIC
+-- profit            NUMERIC
+
+
+
+-- TABLE: people
+
+-- manager           TEXT
+-- region            TEXT
+
+
+-- TABLE: returns
+
+-- order_id          TEXT
+-- returned          TEXT
