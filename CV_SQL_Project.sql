0ecfcdaab83d77bb0db90b3c66bef3bccd6b12de
VitaliMinkov <105166625+VitaliMinkov@users.noreply.github.com>
2026-03-24 11:05:40 +0400

    Update CV_SQL_Project.sql

--- CV_SQL_Project.sql
+++ CV_SQL_Project.sql
@@ -4,12 +4,6 @@
 -- revenue,
 -- profit and profit margin.
 -- Additionally, calculate revenue change MoM (month-over-month)
--- and YoY (year-over-year, compared to the same month las-- 1. Calculate monthly metrics by region:
--- number of orders,
--- number of customers,
--- revenue,
--- profit and profit margin.
--- Additionally, calculate revenue change MoM (month-over-month)
 -- and YoY (year-over-year, compared to the same month last year).
 -- Protect against division by zero.
 
@@ -341,36 +335,3 @@ WHERE EXISTS
            WHERE b.customer_id = a.customer_id)
 GROUP BY a.customer_name
 ORDER BY first_seen_date;
-t year).
--- Protect against division by zero.
-
-
-with monthly_CTE as
-(select to_char((date_trunc('month', order_date)::date),'Month YYYY') as month_label,
-       (date_trunc('month', order_date)::date) as month,
-       region,
-       count(order_id) as orders_cnt,
-       count(customer_id) as customer_cnt,
-       round(sum(sales),2) as sales,
-       round(SUM(profit),2) as profit
-       from orders
-group by month, region
-order by month)
-select month,
-       month_label,
-       region,
-       orders_cnt,
-       customer_cnt,
-       sales,
-       profit,
-       round(profit/(Nullif(sales,0)),2) as margin,
-       sales-lag(sales) over (partition by region order by month) as sales_mom,
-       sales-lag(sales,12) over (partition by region order by month) as sales_yoy
-from monthly_CTE
-order by month, region;
-
-
-
-
-
-
